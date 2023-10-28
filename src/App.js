86cee182b83503cc5e67fceb7139dfd9e5843b9a
Zoey Helmold <zoey@helmittech.com>
2023-10-27 23:51:29 -0700

    test1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+const App = () => {
+    const runbooks = [
+        { name: 'Runbook1', webhookUrl: 'http://webhook1.com' },
+        { name: 'Runbook2', webhookUrl: 'http://webhook2.com' },
+        // ... more runbooks
+    ];
+
+    const executeRunbook = (webhookUrl) => {
+        // Implement POST request logic here
+    };
+
+    return (
+        <div>
+            <h1>My PowerShell Runbook Portal</h1>
+            <ul>
+                {runbooks.map((runbook, index) => (
+                    <li key={index}>
+                        {runbook.name}
+                        <button onClick={() => executeRunbook(runbook.webhookUrl)}>Execute</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
